96a970a668d92b3ebb95b548ab08b73fceb0854a
LauchsuppeSan <guilia.stephan.gs@gmail.com>
2024-05-12 16:21:30 +0200

    mehr grid
    UI sprites für items hinzugefügt. grid adjustable gemacht

---
+++ 6.2 ASP/Assets/Scripts/ItemGrid.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemGrid : MonoBehaviour
+{
+    const float tileSizeWidth = 32;
+    const float tileSizeHeight = 32;
+
+    InventoryItem[,] inventoryItemSlot;
+
+    RectTransform rectTransform;
+
+    [SerializeField] int gridSizeWidth;
+    [SerializeField] int gridSizeHeight;
+
+    private void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        Init(gridSizeWidth, gridSizeHeight);
+    }
+
+    private void Init(int width, int height)
+    {
+        inventoryItemSlot = new InventoryItem[width, height];
+        Vector2 size = new Vector2(width * tileSizeWidth, height * tileSizeHeight);
+        rectTransform.sizeDelta = size;
+    }
+
+    Vector2 positionOnTheGrid = new Vector2();
+    Vector2Int tileGridPosition = new Vector2Int();
+
+    public Vector2Int GetTileGridPosition(Vector2 mousePosition)
+    {
+        positionOnTheGrid.x = mousePosition.x - rectTransform.position.x;
+        positionOnTheGrid.y = rectTransform.position.y - mousePosition.y;
+
+        tileGridPosition.x = (int) (positionOnTheGrid.x / tileSizeWidth);
+        tileGridPosition.y = (int) (positionOnTheGrid.y / tileSizeHeight);
+
+        return tileGridPosition;
+    }
+}
